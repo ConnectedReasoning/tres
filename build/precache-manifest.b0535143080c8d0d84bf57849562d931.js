c823ab8347e775524d63bbd2d3691667ec404ccc
Manuel Hernandez <manuelhe46@gmail.com>
2020-01-15 06:17:24 -0800

    checkin build

---
+++ build/precache-manifest.b0535143080c8d0d84bf57849562d931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623d331368dea6b4a1216fce7a56d9dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "100b52a1c9cc72c4d082",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e3f7767cdf30b8dab54",
+    "url": "/static/js/2.ae149c42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ae149c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "100b52a1c9cc72c4d082",
+    "url": "/static/js/main.7ccf657e.chunk.js"
+  },
+  {
+    "revision": "3c868c8d5d0aa6956743",
+    "url": "/static/js/runtime-main.962184c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
